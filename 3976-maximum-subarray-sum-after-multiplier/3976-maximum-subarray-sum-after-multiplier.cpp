047f9df01cdb5ae69d77d9beea57978e4b269753
tanmay joshi <tanmayjoshi411@gmail.com>
2026-07-10 01:38:52 +0530

    Time: 68 ms (28.81%), Space: 147.1 MB (48.12%) - LeetHub

--- 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
+++ 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
@@ -3,28 +3,30 @@ public:
     typedef long long ll;
     const ll NEG = -4e18;
     long long maxSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        ll ans = LONG_MIN;
-        auto solve = [&](bool flag) { // 1-> multiply 0 -> divide
-            vector<ll> dp( n + 1 , NEG);
-            vector<ll> dp1(n + 1, NEG);
-            vector<ll> dp2(n + 1, NEG);
+        int n =  nums.size();
+        ll ans = NEG;
+        auto solve = [&](bool check){
+            ll dp0 = NEG;
+            ll dp1 = NEG;
+            ll dp2 = NEG;
+            for(auto x : nums){
+                ll mval =( check == true )? x * 1LL *  k : x / k;
+                 
+                ll new_dp0 = max({0LL , dp0 }) + x;
+                ll new_dp1 = max({0LL , dp0 , dp1}) + mval;
+                ll new_dp2 = max({dp1 , dp2}) + x;
 
-            for (int i = 0 ; i < n ; i++) {
-                int x = nums[i];
-                ll Mval = flag == true ? x * 1LL* k : x / k;
+               
+                dp0 = new_dp0;
+                dp1 = new_dp1;
+                dp2 = new_dp2;
 
-                dp[i + 1 ] = max(0LL, dp[i]) + x;  
-                dp1[i + 1] = max({0LL , dp[i] , dp1[i]}) + Mval;
-                dp2[i + 1] = max({dp1[i] , dp2[i]}) + x;
-               ans = max({ ans ,dp[i + 1] , dp1[i + 1] , dp2[i + 1]});
-            
+                ans = max({ans , new_dp0 , new_dp1 , new_dp2}); 
             }
         };
 
         solve(true);
         solve(false);
-
         return ans;
 
     }
