ae2a918c4ab3175aab8db7c24817a372410f018c
tanmay joshi <tanmayjoshi411@gmail.com>
2026-07-12 07:06:16 +0530

    Time: 8 ms (90.26%), Space: 26.3 MB (79.48%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -3,14 +3,13 @@ public:
     
     bool canMakeSubsequence(string s, string t) {
         int n = s.size() , m = s.size();
-
         int  i = 0 , j = 0;
         for(char ch : t){
             if(s[i] == ch)i++;
             i = max(i , j + 1);
             if(s[j] == ch) j++;  
-            if(i >= m || j >= m  ) return true;
+           
         } 
-        return false;
+        return (i >= m || j >= m  );
     }
 };
