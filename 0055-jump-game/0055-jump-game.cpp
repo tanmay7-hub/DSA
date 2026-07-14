399e54f2d296aee6f60120705c8a83d541bfcd06
tanmay joshi <tanmayjoshi411@gmail.com>
2026-07-14 09:45:36 +0530

    Time: 3 ms (27.53%), Space: 52.3 MB (83.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int n = nums.size();
+
+       int curr = 0;
+       int i = 0;
+       for( i ; i < nums.size() ; i++ ){
+         if( i + nums[i] > curr && curr >= i )curr = i + nums[i];
+        //  if(curr < i) return false;
+       }
+
+
+       return curr >= n - 1;
+
+
+    }
+};
