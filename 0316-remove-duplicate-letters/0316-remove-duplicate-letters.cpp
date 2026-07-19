53c991d0de373fb1711fd2b3244cb5dd88d2f498
tanmay joshi <tanmayjoshi411@gmail.com>
2026-07-19 22:40:12 +0530

    Time: 0 ms (100%), Space: 9.8 MB (17.91%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char , int> mp;
+        for(auto ch  :s) mp[ch] ++;
+        stack<char>st;
+        vector<int>taken(26 , false);
+
+        for(auto ch : s){
+            mp[ch ]--;
+            if(taken[ch - 'a']) continue;
+             while(st.size() > 0 ){         
+                if(st.top() > ch && mp[st.top()] > 0){
+                    char top = st.top();
+                    st.pop();
+                    taken[top - 'a'] = false;
+                   
+                }else{
+                      break;
+                }
+             }
+            if(!taken[ch - 'a']){
+                st.push(ch);
+                
+                taken[ch - 'a'] = true;
+            }
+            
+        }
+
+        string ans = "";
+
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(begin(ans) , end(ans));
+        return ans;
+
+    }
+};
