2cd984ac8c4d2e73a499e695bb196730a532157f
tanmay joshi <tanmayjoshi411@gmail.com>
2026-07-20 10:40:04 +0530

    Time: 246 ms (14.33%), Space: 114 MB (12.83%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+     vector<int>par;
+     vector<int>rank;
+    int find(int x ){
+        if(par[x] == x)return x;
+
+        return par[x] = find(par[x]);
+    }
+
+    void unite(int i , int j ){
+        int par_i = find(i);
+        int par_j = find(j);
+
+        if(par_i == par_j ) return ;
+
+        if(rank[par_i] > rank[par_j]){
+             rank[par_i]++;
+             par[par_j] = par_i;
+        }else if(rank[par_i] < rank[par_j]){
+            rank[par_j]++;
+             par[par_i] = par_j;
+        }else{
+               rank[par_i]++;
+               par[par_j] = par_i;  
+        }
+    }
+    void dfs(int root ,vector<bool>&vis, unordered_map<int,vector<int>>&adj){
+        vis[root] = true;
+
+        for(auto child : adj[root]){
+            if(!vis[child]){
+                dfs(child , vis , adj);
+            }
+        }
+
+    }
+    bool check(string s1 , string s2){
+        int diff = 0;
+        for(int i = 0 ; i < s1.size() ; i++ ){
+            if(s1[i] != s2[i]) diff++;
+        }
+        return diff <= 2;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        par.resize(n);
+        rank.resize(n , 0);
+        unordered_map<int , vector<int>>adj;
+        iota(begin(par) , end(par) , 0 );
+
+
+        for(int i = 0 ; i < n ; i++ ){
+            for(int j = i + 1 ; j < n ; j++ ){
+               if(check(strs[i] , strs[j])){
+                   adj[i].push_back(j);
+                   adj[j].push_back(i);
+                   unite(i , j );
+               }
+            }
+        }
+        int ans = 0;
+        vector<bool>visited(n , false);
+        for(int i = 0 ; i < n ; i++ ){
+            if(!visited[i]){
+                dfs(i,visited,adj);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
