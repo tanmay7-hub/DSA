aa15ec9f2c9ae5051936be74d348b5e95ef401bf
tanmay joshi <tanmayjoshi411@gmail.com>
2026-08-11 00:32:55 +0530

    Time: 39 ms (67.82%), Space: 14.1 MB (54.42%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[100000];
+    bool solve(int n ){
+        if(n == 0) return false;
+
+        if(dp[n] != -1)return dp[n];
+        for(int i = 1 ; i * i <= n ; i++){
+            if( !( solve( n - (i * i)))){
+                return dp[n] = true;
+            }
+        }
+        return dp[n] = false;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp , -1 , sizeof(dp));
+        return solve(n);
+    }
+};
