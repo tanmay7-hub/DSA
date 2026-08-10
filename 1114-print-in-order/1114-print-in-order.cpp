19a21191563cd7562c6198d6a2eedef88cecfd2b
tanmay joshi <tanmayjoshi411@gmail.com>
2026-08-10 19:31:02 +0530

    Time: 6 ms (54.47%), Space: 9.5 MB (47.87%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+class Foo {
+    mutex mtx;
+    condition_variable cv;
+    int turn ;
+public:
+    
+    Foo() {
+        turn = 0;
+     }
+
+    void first(function<void()> printFirst) {
+    
+        printFirst();
+        turn = 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+
+        unique_lock< mutex > lock(mtx) ;
+        while(turn != 1){
+            cv.wait(lock);
+        }      
+        printSecond();
+        turn = 2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        
+        unique_lock< mutex> lock(mtx);
+
+        while(turn != 2){
+            cv.wait(lock);
+        }
+        printThird();
+        turn = 3;
+        cv.notify_all();
+    }
+};
