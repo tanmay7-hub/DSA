eddab57f89cd19fd01a407583d272863528a44f6
tanmay joshi <tanmayjoshi411@gmail.com>
2026-09-12 00:39:55 +0530

    Time: 7 ms (37.02%), Space: 22.5 MB (36.93%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[30001][2];
+    int solve(vector<int>& prices, int fee, int idx, int buy) {
+        if (idx >= prices.size())
+            return 0;
+
+        if (dp[idx][buy] != -1)
+            return dp[idx][buy];
+        if (buy) {
+            return dp[idx][buy] =
+                       max(-prices[idx] + solve(prices, fee, idx + 1, !buy),
+                           solve(prices, fee, idx + 1, buy));
+        }
+        return dp[idx][buy] =
+                   max(prices[idx] - fee + solve(prices, fee, idx + 1, !buy),
+                       solve(prices, fee, idx + 1, buy));
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp , -1 , sizeof(dp));
+        return solve(prices , 0 , 0 , 1);
+    }
+};
